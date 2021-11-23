befb1d37d0961354cc96c71273e026c5baaf5dae
ambekar123 <tarunhambekar@gmail.com>
2021-11-24 00:04:13 +0530

    assignment middleware2 23rdNov

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,13 +2,13 @@ const userModel = require('../models/userModel')
 
 const createUser = async function (req, res) {
     let userDetails = req.body
-    // let appType = req.headers['isfreeapp']
-    // let userType
-    // if(appType === 'false') {
-    //     userType = false
-    // } else {
-    //     userType = true
-    // }
+     let appType = req.headers['isfreeapp']
+     let userType
+     if(appType === 'false') {
+        userType = false
+     } else {
+         userType = true
+     }
     
     userDetails.freeAppUser = req.isFreeAppUser//this attribute was set in req in the appMiddleware
     let userCreated = await userModel.create(userDetails)
